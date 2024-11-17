093ba7e65ed77f39ac55607cdb792b17e8d663cf
Anuj Gupta <65494292+Anuj-afk@users.noreply.github.com>
2024-11-17 18:00:59 +0530

    test 4

--- insta_clone/src/App.jsx
+++ insta_clone/src/App.jsx
@@ -1,32 +1,38 @@
+// App.js
+import React, { createContext, useEffect, useState } from "react";
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/sidebar.Component";
+import HomePage from "./pages/homePage";
+import UserAuthForm from "./pages/userAuthForm.page";
+import ProfilePage from "./pages/ProfilePage";
+import { lookInSession } from "./common/session";
+import ProtectedRoutes from "./components/ProtectedRoute.component";
 
+
+export const UserContext = createContext({})
 const App = () => {
-  return (
-    <UserContext.Provider value={{ userAuth, setUserAuth }}>
-      <Router>
+    const [userAuth, setUserAuth] = useState({});
+
+    useEffect(() => {
+        let userInSession = lookInSession("user");
+        userInSession ? setUserAuth(JSON.parse(userInSession)) : setUserAuth({accessToken: null});
+    }, [])
+
+    return (
+      <UserContext.Provider value={{ userAuth, setUserAuth }}>
         <Routes>
           <Route path="/Insta-Clone" element={<Navbar />}>
-            <Route
-              index
-              element={
-                <ProtectedRoutes>
-                  <HomePage />
-                </ProtectedRoutes>
-              }
-            />
-            <Route
-              path="profile/:id"
-              element={
-                <ProtectedRoutes>
-                  <ProfilePage />
-                </ProtectedRoutes>
-              }
-            />
+              <Route index element={<ProtectedRoutes> <HomePage /> </ProtectedRoutes>}/>
+              <Route path="profile/:id" element={<ProtectedRoutes> <ProfilePage /></ProtectedRoutes>}/>
+              {/* <Route index element={<HomePage />}/>
+              <Route path="profile/:id" element={<ProfilePage />}/> */}
           </Route>
           <Route path="/Insta-Clone/signin" element={<UserAuthForm type="sign-in" />} />
           <Route path="/Insta-Clone/signup" element={<UserAuthForm type="sign-up" />} />
         </Routes>
-      </Router>
-    </UserContext.Provider>
-  );
-};
+      </UserContext.Provider>
+    );
+  };
+  
+
+export default App;
