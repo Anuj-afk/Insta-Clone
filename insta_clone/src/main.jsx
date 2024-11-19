5c48dab0330d1737d3039c61ae4f55d84e6677a0
Anuj Gupta <65494292+Anuj-afk@users.noreply.github.com>
2024-11-19 19:44:42 +0530

    base router changed

--- insta_clone/src/main.jsx
+++ insta_clone/src/main.jsx
@@ -1,11 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename="/Insta-Clone"> {/* Wrap App with BrowserRouter here */}
-    <App />
-  </BrowserRouter>
-)
+createRoot(document.getElementById("root")).render(
+    <BrowserRouter>
+        {/* Wrap App with BrowserRouter here */}
+        <App />
+    </BrowserRouter>
+);
+//   <BrowserRouter basename="/Insta-Clone"> {/* Wrap App with BrowserRouter here */}
+//     <App />
+//   </BrowserRouter>
+// )
