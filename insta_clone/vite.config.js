2050d02d1c943dd83b063def5e357489234812ac
Anuj Gupta <65494292+Anuj-afk@users.noreply.github.com>
2024-11-19 20:36:20 +0530

    404 error

--- insta_clone/vite.config.js
+++ insta_clone/vite.config.js
@@ -1,8 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Insta-Clone/",
-})
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined, // Optional: single file builds
+      },
+    },
+  },
+  base: '/Insta-clone/', // Ensure this matches your repository name
+});
